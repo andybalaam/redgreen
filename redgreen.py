67996875f7e4c2463fc8bbf3a2665432c3e659bb
Andy Balaam <andybalaam@artificialworlds.net>
2012-10-18 08:42:47 +0100

    ready-screen

--- redgreen.py
+++ redgreen.py
@@ -2,14 +2,27 @@
 
 import pygame
 
-screen_size = 640, 480
+screen_width = 640
+screen_height = 480
+
+screen = None
+ready_text = None
 
 def start():
+    global screen, ready_text
     pygame.init()
-    screen = pygame.display.set_mode( screen_size )
+    screen = pygame.display.set_mode( ( screen_width, screen_height ) )
+    font = pygame.font.Font( None, screen_height / 5 )
+    ready_text = font.render( "Ready?", 1, pygame.Color( "white" ) )
 
 def ready_screen():
-    pass
+    textpos = ready_text.get_rect(
+        centerx = screen.get_width() / 2,
+        centery = screen.get_height() / 2
+    )
+
+    screen.blit( ready_text, textpos )
+    pygame.display.flip()
 
 def wait():
     pass
