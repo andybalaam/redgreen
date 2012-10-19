316d3b783d4ca9e1551add910440f20de27d0dff
Andy Balaam <andybalaam@artificialworlds.net>
2012-10-19 05:47:05 +0100

    Make ready screen explicitly black, and shape screen white

--- redgreen.py
+++ redgreen.py
@@ -10,27 +10,27 @@ screen = None
 ready_text = None
 
 def start():
-    global screen, ready_text
+    global screen, ready_text, ready_text_pos
     pygame.init()
     screen = pygame.display.set_mode( ( screen_width, screen_height ) )
     font = pygame.font.Font( None, screen_height / 5 )
     ready_text = font.render( "Ready?", 1, pygame.Color( "white" ) )
-
-def ready_screen():
-    textpos = ready_text.get_rect(
+    ready_text_pos = ready_text.get_rect(
         centerx = screen.get_width() / 2,
         centery = screen.get_height() / 2
     )
 
-    screen.blit( ready_text, textpos )
+def ready_screen():
+    screen.fill( pygame.Color( "black" ) )
+    screen.blit( ready_text, ready_text_pos )
     pygame.display.flip()
 
 def wait():
-    time_to_wait = random.randint( 2000, 5000 ) # Between 2 and 5 seconds
+    time_to_wait = random.randint( 1500, 3000 ) # Between 1.5 and 3 seconds
     pygame.time.wait( time_to_wait ) # Note bug: can't quit during this time
 
 def shape():
-    screen.fill( pygame.Color( "black" ) )
+    screen.fill( pygame.Color( "white" ) )
     pygame.display.flip()
 
 def end():
